6abf1a7e790af049045c1f69e008295da51fd1f8
Danglh2411 <danglehai2411@gmail.com>
2017-03-11 10:57:05 +0700

    Google cloud platform Hello World

---
+++ Nhom2HelloWorld/src/com/haidangle/Nhom2HelloWorldServlet.java
@@ -0,0 +1,12 @@
+package com.haidangle;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Nhom2HelloWorldServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
